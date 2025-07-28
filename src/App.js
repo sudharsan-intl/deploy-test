e91b5e4ff906bb0e74ebdf7b334d31fdef363d41
sudharsan-intl <sudharsan-intl@gmail.com>
2025-07-28 14:29:49 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='text-white'>
+      <h1>Frontend Developer Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
